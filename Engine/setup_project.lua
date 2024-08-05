a6f37a1a456ad7e23e4fb6db436d8eea1d7cf15a
YevhenHoina <yevhenhoina@gmail.com>
2024-08-05 12:44:48 +0300

    project set up

---
+++ Engine/setup_project.lua
@@ -0,0 +1,66 @@
+project "kubykEngine"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "Binaries/%{cfg.buildcfg}"
+   staticruntime "off"
+   
+   files { "Source/**.h",
+   "Source/**.cpp",
+   "Source/**.c",
+   "Source/**.fs",
+   "Source/**.vs",
+   "Source/**.py"
+    }
+   includedirs { 
+    ".\\Inсlude\\assimp",
+    ".\\Inсlude\\GL",
+    ".\\Inсlude\\GLFW",
+    ".\\Inсlude\\ImGui"
+   }
+   libdirs { "$(SolutionDir)Engine/Libraries" }
+   
+
+   
+      
+   defines
+   {
+       "_CRT_SECURE_NO_WARNINGS"
+   }
+
+
+   targetdir ("$(SolutionDir)Engine/Build/" .. OutputDir .. "/%{prj.name}")
+   objdir ("$(SolutionDir)Engine/Build/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   links {
+   "glfw3.lib",
+   "opengl32.lib",
+   "user32.lib",
+   "gdi32.lib",
+   "shell32.lib",
+   "assimp-vc143-mt.lib"
+   }
+   
+   filter "system:windows"
+       systemversion "latest"
+       defines { "WINDOWS" }
+    
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+       debugenvs { "PATH=%PATH%;$(SolutionDir)\\Engine\\Binaries\\Debug" }
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+       debugenvs { "PATH=%PATH%;$(SolutionDir)\\App\\Build\\Binaries\\Release" }
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
+       debugenvs { "PATH=%PATH%;$(SolutionDir)\\App\\Build\\Binaries\\Dist" }
