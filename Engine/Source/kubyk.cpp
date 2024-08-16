59321c47f285b5aa8266fe701f55fdf7854cfe8f
yevhenhoina <yevhenhoina@gmail.com>
2024-08-16 17:52:59 +0300

    init glad and linmath

---
+++ Engine/Source/kubyk.cpp
@@ -0,0 +1,71 @@
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include "linmath.h"
+
+#include <iostream>
+
+
+typedef struct Vertex
+{
+    vec2 pos;
+    vec3 col;
+} Vertex;
+
+static const Vertex vertices[3] =
+{
+    { { 0.7f, 0.9f}, {1.0f, 0.0f, 0.0f} },
+    { {0.0f, -0.8f}, {0.0f, 1.0f, 0.0f} },
+    { {-0.7f, 0.9f}, {0.0f, 0.0f, 1.0f} }
+};
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "MyApp", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize Glad" << std::endl;
+        return -1;
+    }
+
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.9f, 0.8f, 0.0f, 1.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
