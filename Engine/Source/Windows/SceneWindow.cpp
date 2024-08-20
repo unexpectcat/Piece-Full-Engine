a2a1fac9dfe10f3423beb7ee152ee4c92fa4b335
yevhenhoina <yevhenhoina@gmail.com>
2024-08-20 17:58:03 +0300

    an implementation of openGL window inside imgui app

--- Engine/Source/Windows/SceneWindow.cpp
+++ Engine/Source/Windows/SceneWindow.cpp
@@ -31,9 +31,7 @@ int SceneWindow(void)
     if (!glfwInit())
         return -1;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "MyApp", NULL, NULL);
@@ -67,7 +65,7 @@ int SceneWindow(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    
     glfwTerminate();
     return 0;
 }
---
+++ Engine/Source/Windows/testWindow.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+void InitFramebuffer(int width, int height);
+
+void RenderToFramebuffer();
+
+GLuint GetRenderedTexture();
