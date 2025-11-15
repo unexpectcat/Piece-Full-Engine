2f3f00af3ddff63a2562dce04ab4d218c16974e4
YevhenHoina <yevhenhoina@gmail.com>
2025-11-16 01:02:54 +0200

    redefined architecture

---
+++ premake.lua
@@ -0,0 +1,78 @@
+-- premake5.lua
+workspace "sausage"
+architecture "x64"
+configurations { "Debug", "Release", "Dist" }
+startproject "kubykEngine"
+
+-- Workspace-wide build options for MSVC
+filter "system:windows"
+buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+project "part1"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++20"
+targetdir "bin/%{cfg.buildcfg}"
+staticruntime "off"
+
+files { "src/**.h",
+    "src/**.cpp",
+    "src/**.c",
+    "src/**.fs",
+    "src/**.vs",
+    "src/**.py"
+}
+includedirs {
+    "$(SolutionDir)include\\assimp",
+    "$(SolutionDir)include\\glad",
+    "$(SolutionDir)include\\GLFW",
+    "$(SolutionDir)include\\ImGui",
+    "$(SolutionDir)include"
+}
+libdirs { "$(SolutionDir)bin" }
+
+
+
+
+defines
+{
+    "_CRT_SECURE_NO_WARNINGS"
+}
+
+
+targetdir("$(SolutionDir)bin/" .. OutputDir .. "/%{prj.name}")
+objdir("$(SolutionDir)bin/" .. OutputDir .. "/%{prj.name}")
+
+links {
+    "glfw3.lib",
+    "opengl32.lib",
+    "user32.lib",
+    "gdi32.lib",
+    "shell32.lib"
+}
+
+filter "system:windows"
+systemversion "latest"
+defines { "WINDOWS" }
+
+filter "configurations:Debug"
+defines { "DEBUG" }
+runtime "Debug"
+symbols "On"
+debugenvs { "PATH=%PATH%;$(SolutionDir)\\bin\\debug" }
+
+filter "configurations:Release"
+defines { "RELEASE" }
+runtime "Release"
+optimize "On"
+symbols "On"
+debugenvs { "PATH=%PATH%;$(SolutionDir)\\bin\\release" }
+
+filter "configurations:Dist"
+defines { "DIST" }
+runtime "Release"
+optimize "On"
+symbols "Off"
+debugenvs { "PATH=%PATH%;$(SolutionDir)\\bin\\dist" }
